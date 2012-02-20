310e55984a8d18a4e09dfa7cef0f2fc34375ce72
alta189 <stephendwilliams7@gmail.com>
2012-02-20 09:38:14 -0500

    Initial Plugin Loading

--- src/main/java/com/alta189/cyborg/Cyborg.java
+++ src/main/java/com/alta189/cyborg/Cyborg.java
@@ -19,12 +19,57 @@
 
 package com.alta189.cyborg;
 
+import com.alta189.cyborg.api.event.EventManager;
+import com.alta189.cyborg.api.event.SimpleEventManager;
+import com.alta189.cyborg.api.plugin.CommonPluginLoader;
+import com.alta189.cyborg.api.plugin.CommonPluginManager;
+import com.alta189.cyborg.api.plugin.Plugin;
+import com.alta189.cyborg.api.plugin.PluginManager;
+
 import java.io.File;
 
 public class Cyborg {
+	private static Cyborg instance;
+	private final File pluginDir = new File("plugins");
+	private final CommonPluginManager pluginManager;
+	private final SimpleEventManager eventManager;
+	
+
+	public Cyborg() {
+		if (instance != null)
+			throw new IllegalAccessError("There is already an instance of Cyborg!");
+		pluginManager = new CommonPluginManager(this);
+		pluginManager.registerPluginLoader(CommonPluginLoader.class);
+		eventManager = new SimpleEventManager();
+		instance = this;
+	}
+
+	public static Cyborg getInstance() {
+		return instance;
+	}
+
+	private final void loadPlugins() {
+		if (!pluginDir.exists())
+			pluginDir.mkdirs();
+		pluginManager.loadPlugins(pluginDir);
+	}
+
+	private final void enablePlugins() {
+		for (Plugin plugin : pluginManager.getPlugins()) {
+			pluginManager.enablePlugin(plugin);
+		}
+	}
+	
+	public EventManager getEventManager() {
+		return eventManager;
+	}
+	
+	public PluginManager getPluginManager() {
+		return pluginManager;
+	}
 
 	public File getUpdateFolder() {
-		return null;
+		return new File(pluginDir, "updates");
 	}
 
 }
