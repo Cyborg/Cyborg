b9707c8291679f4c677ec614db3a5e381423f6b6
alta189 <stephendwilliams7@gmail.com>
2012-05-15 00:23:11 -0400

    Add CollectionUtil

---
+++ src/main/java/com/alta189/cyborg/api/util/CollectionUtil.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2012 CyborgDev <cyborg@alta189.com>
+ *
+ * This file is part of cyborg
+ *
+ * cyborg is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * cyborg is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.alta189.cyborg.api.util;
+
+import java.util.Collection;
+import java.util.regex.Pattern;
+
+public class CollectionUtil {
+	public static boolean contains(Collection<String> collection, String regex) {
+		Pattern pattern = Pattern.compile(regex);
+
+		for (String s : collection) {
+			if (pattern.matcher(s).matches()) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+}
